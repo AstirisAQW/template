666b6b7c2c6265a4d6eb9245ff0601f4fd977275
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-06-19 11:26:29 +0800

    removed plant related files

--- src/domain/entities/plant.ts
+++
@@ -1,8 +0,0 @@
-export class PlantEntity {
-    private id: number
-    private name: string
-    constructor(id: number, name: string) {
-        this.id = id;
-        this.name = name;
-    }
-}
--- src/domain/repositories/plant.ts
+++
@@ -1,6 +0,0 @@
-import { PlantEntity } from "../entities/plant";
-
-export interface PlantRepository {
-    getPlants(): Promise<PlantEntity[]>;
-    addPlant(entity: PlantEntity): Promise<PlantEntity>;
-}
