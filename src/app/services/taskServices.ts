96fb34b5e561b7906b264c0bba3402ab21878346
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-06-19 14:58:27 +0800

    renamed variables

--- src/app/services/taskServices.ts
+++ src/app/services/taskServices.ts
@@ -1,13 +1,12 @@
 import { TaskRepository } from "../../domain/repositories/TaskRepository";
-import { InMemoryTaskRepository } from "../../data/InMemoryTaskRepository";
-import { LocalStorageTaskRepository } from "../../data/LocalStorageTaskRepository";
+import { InMemoryTaskRepository } from "../../data/InMemory_TaskRepository";
+import { LocalStorageTaskRepository } from "../../data/LocalStorage_TaskRepository";
 
-import { AddTaskUsecase } from "../../domain/usecases/add_task_usecase";
-import { GetAllTasksUsecase } from "../../domain/usecases/get_all_task_usecase";
-import { RemoveTaskUsecase } from "../../domain/usecases/remove_task_usecase";
-import { UpdateTaskUsecase } from "../../domain/usecases/update_task_usecase";
-import { GetTaskUsecase } from "../../domain/usecases/get_task_usecase";
-import { TaskEntity } from "../../domain/entities/TaskEntity";
+import { AddTask_Usecase } from "../../domain/usecases/AddTask_Usecase";
+import { GetAllTask_Usecase } from "../../domain/usecases/GetAllTask_UseCase";
+import { RemoveTask_Usecase } from "../../domain/usecases/RemoveTask_Usecase";
+import { UpdateTask_Usecase } from "../../domain/usecases/UpdateTask_Usecase";
+import { GetTask_Usecase } from "../../domain/usecases/GetTask_Usecase";
 
 // --- Configuration for Switching Repository ---
 // Change this to false to use InMemoryTaskRepository
@@ -25,11 +24,11 @@ if (USE_LOCAL_STORAGE_FOR_TASKS) {
 }
 
 // Instantiate use cases with the chosen repository instance
-export const addTaskUsecase = new AddTaskUsecase(taskRepositoryInstance);
-export const getAllTasksUsecase = new GetAllTasksUsecase(taskRepositoryInstance);
-export const removeTaskUsecase = new RemoveTaskUsecase(taskRepositoryInstance);
-export const updateTaskUsecase = new UpdateTaskUsecase(taskRepositoryInstance); // We'll prepare it even if UI doesn't use it yet
-export const getTaskUsecase = new GetTaskUsecase(taskRepositoryInstance); // We'll prepare it
+export const AddTask_Service = new AddTask_Usecase(taskRepositoryInstance);
+export const GetAllTask_Service = new GetAllTask_Usecase(taskRepositoryInstance);
+export const RemoveTask_Service = new RemoveTask_Usecase(taskRepositoryInstance);
+export const UpdateTask_Service = new UpdateTask_Usecase(taskRepositoryInstance); // We'll prepare it even if UI doesn't use it yet
+export const GetTask_Service = new GetTask_Usecase(taskRepositoryInstance); // We'll prepare it
 
 // Helper type for params, can also be defined in usecase files
 export type AddTaskParams = { title: string };
