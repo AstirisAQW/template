3c80d4e96fa2a096f022b808f08f72917507f6f3
AstirisAQW <deysolong.josephjose457@gmail.com>
2025-06-24 15:32:38 +0800

    added completed variable

--- src/domain/entities/TaskEntity.ts
+++ src/domain/entities/TaskEntity.ts
@@ -1,9 +1,11 @@
 export class TaskEntity {
     public id: number;
     public content: string;
+    public completed: boolean;
 
-    constructor(id: number, content: string) {
+    constructor(id: number, content: string, completed:boolean) {
         this.id = id;
         this.content = content;
+        this.completed = completed;
     }
 }
